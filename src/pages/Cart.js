b80b4a801b159ad72ac379936030fbcbbd2676bd
mosesmramba <moses.mramba@student.moringaschool.com>
2023-11-09 19:49:14 +0300

    updated commit

---
+++ src/pages/Cart.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ShoppingCart from '../components/ShoppingCart'
+
+export default function Cart() {
+  return (
+    <div>
+      <ShoppingCart/>
+    </div>
+  )
+}
